450cd0196693be927a3fb97db78ebd7d8667b98f
Iris <agboolaisholaidreez>
2022-11-09 09:51:30 +0100

    update

--- src/components/molecules/shoppingCartCard/index.tsx
+++ src/components/molecules/shoppingCartCard/index.tsx
@@ -1,29 +1,30 @@
 import React from "react";
 import { BeakerIcon, PlusIcon, MinusIcon } from "@heroicons/react/24/outline";
-interface ShoppingCardCardProps {
-  imageSrc: string;
-  imageAlt: string;
-  name: string;
-  quantity: number;
-  price: string;
-  href: string;
-  color: string;
+import { ProductType } from "../../../@types/reducer/shoppingCart";
+import { currencyFormatter } from "../../../helpers/currencyFormatter";
+interface ShoppingCartCardProps extends ProductType {
+  onClickRemove?: () => void;
+  onClickIncrease?: () => void;
+  onClickDescrease?: () => void;
 }
 function ShoppingCartCard({
-  imageSrc,
-  imageAlt,
+  image,
   href,
   name,
   quantity,
   price,
   color,
-}: ShoppingCardCardProps) {
+  onClickDescrease,
+  onClickRemove,
+  onClickIncrease,
+  packagePrice,
+}: ShoppingCartCardProps) {
   return (
     <li className="flex py-6">
       <div className="h-24 w-24 md:h-36 md:w-36 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
         <img
-          src={imageSrc}
-          alt={imageAlt}
+          src={image}
+          alt={name}
           className="h-full w-full object-cover object-center"
         />
       </div>
@@ -34,25 +35,35 @@ function ShoppingCartCard({
             <h3>
               <a href={href}>{name}</a>
             </h3>
-            <p className="ml-4">{price}</p>
+            <p className="ml-4">{currencyFormatter(packagePrice, "NGN")}</p>
           </div>
           <p className="mt-1 text-sm text-gray-500">{color}</p>
         </div>
         <div className="flex flex-1 items-end justify-between text-sm">
           <div className="flex items-center">
-            <div className="bg-red-600 h-[20px] w-[20px] rounded flex justify-center items-center cursor-pointer hover:bg-red-700">
+            <button
+              disabled={quantity < 1}
+              onClick={() => onClickDescrease && onClickDescrease()}
+              className="bg-red-600 h-[20px] w-[20px] disabled:bg-gray-200 rounded flex justify-center items-center cursor-pointer hover:bg-red-700"
+            >
               <MinusIcon className="w-3 text-white" />
-            </div>
-            <p className="text-gray-500 p-2 ">Qty {quantity}</p>
-            <div className="bg-green-600 h-[20px] w-[20px] rounded flex justify-center items-center cursor-pointer hover:bg-green-700">
+            </button>
+            <p className="text-gray-500 p-2  text-center min-w-[70px]">
+              Qty {quantity}
+            </p>
+            <button
+              onClick={() => onClickIncrease && onClickIncrease()}
+              className="bg-green-600 h-[20px] w-[20px]  rounded flex justify-center items-center cursor-pointer hover:bg-green-700"
+            >
               <PlusIcon className="w-3 text-white" />
-            </div>
+            </button>
           </div>
 
           <div className="flex">
             <button
               type="button"
               className="font-medium text-indigo-600 hover:text-indigo-500"
+              onClick={() => onClickRemove && onClickRemove()}
             >
               Remove
             </button>
---
+++ src/database/productCart.ts
@@ -0,0 +1,50 @@
+import { ProductType } from "../@types/reducer/shoppingCart";
+
+const productsCart: ProductType[] = [
+  {
+    id: 1,
+    name: "Throwback Hip Bag",
+    href: "#",
+    color: "Salmon",
+    price: 90.0,
+    quantity: 1,
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg",
+    packagePrice: 90.0,
+  },
+  {
+    id: 2,
+    name: "Medium Stuff Satchel",
+    href: "#",
+    color: "Blue",
+    price: 32.0,
+    quantity: 1,
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
+    packagePrice: 32.0,
+  },
+  {
+    id: 3,
+    name: "Throwback Hip Bag",
+    href: "#",
+    color: "Salmon",
+    price: 90.0,
+    quantity: 1,
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg",
+    packagePrice: 90.0,
+  },
+  {
+    id: 4,
+    name: "Medium Stuff Satchel",
+    href: "#",
+    color: "Blue",
+    price: 32.0,
+    quantity: 1,
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
+    packagePrice: 32.0,
+  },
+];
+
+export default productsCart;
